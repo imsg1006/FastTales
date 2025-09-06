6fd8ad2497b292c82dada3db48aa557024b3c3f7
Shlok Gupta <shlokgupta10@gmail.com>
2025-09-06 07:35:08 +0530

    fixed typewriter color

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -2,22 +2,21 @@
 import { TypewriterEffectSmooth } from "../components/ui/typewriter-effect";
 export function TypewriterEffectSmoothDemo() {
   const words = [
-    {
-      text: "Interactive",
-      className:"text-pink-400  "
-    },
-    {
-      text: "Story",
-      className:"text-pink-400 mr-5"
-    },
-    {
-      text: "Generator",
-      className: "text-purple-500 dark:text-blue-500"
-    },
-     
-  ];
+     {
+    text: "Interactive",
+    className: "word-interactive",
+  },
+  {
+    text: "Story",
+    className: "word-story",
+  },
+  {
+    text: "Generator",
+    className: "word-generator",
+  },
+];
   return (
-    <div className="flex flex-col items-center justify-center  ">
+    <div className="flex  items-center justify-center  ">
     
       <TypewriterEffectSmooth className="word-spacing" words={words} />
       
