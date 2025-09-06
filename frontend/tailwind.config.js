2285b85c3c9924bd38249ae8db01a25899c1f6e2
Shlok Gupta <shlokgupta10@gmail.com>
2025-09-06 08:01:49 +0530

    fixed typewriter effect

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    "inline-block",
+    "animate-typewriter",
+    "animate-cursor",
+    "text-pink-400",
+    "text-purple-500",
+    "dark:text-blue-500",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
