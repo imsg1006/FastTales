5151dcf9585f699d67eb2b90b1f1178e08cd22b6
Shlok Gupta <shlokgupta10@gmail.com>
2025-09-07 03:22:30 +0530

    edited main.py

--- backend/main.py
+++ backend/main.py
@@ -29,5 +29,5 @@ app.include_router(job.router, prefix=settings.API_PREFIX)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app  , host="0.0.0.0", port=8000 , reload=True )
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
