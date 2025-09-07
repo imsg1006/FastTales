a3aa33b803ba7363ea2a8a75402464412dded91c
Shlok Gupta <shlokgupta10@gmail.com>
2025-09-07 08:19:31 +0530

    changed the api_url

--- frontend/utils.js
+++ frontend/utils.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "/choreo-apis/backend-3888913110:8000/api";
+export const API_BASE_URL = "https://7ebb8167-0533-45a6-9216-9f5622418d70-dev.e1-us-east-azure.choreoapis.dev/fasttales/backend/v1.0/api";
